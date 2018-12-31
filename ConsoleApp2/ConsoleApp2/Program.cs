6d44e42d955f5d7c561e4f92889deaffc1b4af35
AuyeskhanM <auyeskhanm@gmail.com>
2018-12-31 19:15:14 +0600

    prosto tak

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте " + name);
+            Console.ReadLine();
+        }
+    }
+}
